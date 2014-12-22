7ea46f6d8bc4e0cafcdb06eb9731b06fdf77c51b
Jay Daley <jay@controlfreak.net>
2014-12-23 09:37:47 +1300

    Initial commit
    Some roughly working code

---
+++ README.md
@@ -0,0 +1,73 @@
+# Entropy Server
+
+A JSON API that serves entropy.
+
+entropyserver is a node.js server that serves requests for entropy over a REST API.  The entropy comes from a hardware random source directly attached to the server.  Currently the only support device is a ComScire PQ32MU, which outputs random bytes at 32Mb/s.
+
+The clients (unfinished) update /dev/random automatically by pulling data from the server.
+
+This is ALPHA software but under active development.
+
+## Security
+
+This is still alpha software so the security model is likely to develop.  For now the security of the system is provided by:
+
+1. The client is configured to use a specific entropy server and no discovery protocol is included (other than DNS).
+1. Communication with the client is over HTTPS (not yet implemented)
+1. The server certificate is distributed and expected to be installed with the client for the client to use.  There is no trust of X.509 Certificate Authorities. (not yet implemented)
+1. The domain name of the server is DNSSEC signed, from the root down.(not yet implemented)
+
+## Config file
+
+Configuration is in a JSON text file called entropyserver.json and example of which is:
+
+    {
+      "server": "entropy://entropy.net.nz/",
+      "protocolversions": ["1.0"],
+      "source": "ComScire PQ32MU",
+      "bitsofentropy": 8,
+      "minrequestbytes": 64,
+      "maxrequestbytes": 4096,
+      "ratelimits": [ {"period": 1000, "requests": 1, "bytes": 4096} ]
+    }
+
+The elements are:
+
++ *server* The URL of this server.  Should match the URL by which clients contact this server as it may be used as a verification item. (Required)
++ *protocolversions* List of protocol versions that this server supports.  Each version is given as a string. (Required)
++ *source* String that identifies the device that generates the entropy used by this server. (Required)
++ *bitsofentropy* Number of bits of entropy per byte of data provided. (Required)
++ *minrequestbytes* Minimum number of bytes that can be requested in a single request. (Optional, defaults to 64)
++ *maxrequestbytes* Maximum number of bytes that can be requested in a single request. (Optional, defaults to 4096)
++ *ratelimits* List of objects that define the rate limits enforced by this server.  (Optional, defaults to empty list) (not yet implemented)
+++ *period* Period of time in milliseconds. (Required)
+++ *requests* Total number of requests allowed within the period. (Required)
+++ *bytes* Total number of bytes allowed to be requested within the period. (Required)
++ *buffersize*  How mamy bytes of entropy the server should buffer from the device. (Optional, defaults to 67108864)
++ *port* Port the server listens on.  (Optional, defaults to 11372)
+
+## API
+
+The default port the service is accessible on is:
+
+    11372
+
+The root of the interface is:
+
+    /api/
+
+Information on the server can be found at:
+
+    /api/info/
+
+_example output:_
+
+    {"server":"entropy://entropy.net.nz/","protocolversions":["1.0"],"source":"ComScire PQ32MU","bitsofentropy":8,"minrequestbytes":64,"maxrequestbytes":4096,"ratelimits":[]}
+
+Entropy is requested by 
+
+	/api/entropy?bytes=64
+
+_example output:_
+
+    {"bytes":64,"entropy":[191,235,79,33,150,135,6,94,78,105,69,134,154,10,101,67,145,37,191,2,202,69,4,166,16,229,182,132,178,61,222,82,135,146,124,195,2,43,61,196,108,243,223,75,107,20,135,162,29,253,44,72,102,224,89,69,173,20,187,174,201,25,183,21]}
---
+++ entropyserver.js
@@ -0,0 +1,288 @@
+// entropyserver.js  A JSON API that serves entropy
+//
+// Copyright (c) 2014 NZ Registry Services
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Affero General Public License for more details.
+
+// You should have received a copy of the GNU Affero General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"use strict";
+
+/*** SETUP  ***/
+
+// import packages
+var ftdi        = require('ftdi');
+var express     = require('express');
+var fs          = require('fs');
+
+// config and global variables
+var DEFAULT_MINREQUESTBYTES = 64;
+var DEFAULT_MAXREQUESTBYTES = 4096;
+var DEFAULT_PORT = 11372;
+var DEFAULT_BUFFERSIZE = 67108864;
+
+var config = loadconfig();
+var app = express();                                          // define our app using express
+var router = express.Router();                                // get an instance of the express router
+var circ = new CircularBuffer(config.buffersize);             // the buffer to hold the random bytes
+
+// initiaisation
+initialiseRNG();
+setuprouter();
+
+// start the server
+app.listen(config.port);
+console.log("Entropy server started on port " + config.port.toString());
+
+
+/*** ROUTES ***/
+
+function setuprouter() {
+  // base route that nobody should be using (accessed at GET http://localhost:11372/api)
+  router.get("/", function(req, res) {
+    res.json({ message: "RNG server" }); 
+  });
+
+  // server info route
+  router.get("/info/", function(req, res) {
+    res.json( { server: config.server, 
+                protocolversions: config.protocolversions,
+                source: config.source,
+                bitsofentropy: config.bitsofentropy,
+                minrequestbytes: config.minrequestbytes,
+                maxrequestbytes: config.maxrequestbytes,
+                ratelimits: config.ratelimits });
+  });
+
+  // main API route
+  router.get("/entropy/", function(req, res) {
+    var bytes = req.query.bytes;
+    var d;
+
+    bytes = parseInt(bytes);
+    console.log("bytes requested: " + bytes.toString());
+    bytes = (bytes < config.minrequestbytes) ? config.minrequestbytes : bytes;
+    bytes = (bytes > config.maxrequestbytes) ? config.maxrequestbytes : bytes;
+
+    d = circ.pop(bytes);
+
+    res.json({ bytes: bytes, entropy: d});
+  });
+
+  // register routes with /api prefix
+  app.use('/api', router);
+}
+
+
+/*** CONFIG ***/
+
+function loadconfig() {
+  var cfgjson = fs.readFileSync('./entropyserver.json');
+  var cfg;
+
+  try {
+    cfg = JSON.parse(cfgjson);                                // parse the JSON file
+  }
+  catch (err) {
+    console.log("Error parsing config file");
+    console.log(err);
+    process.exit(1);                                          // exit with a non-sero error code TODO make that a global variable
+  }
+
+  checkconfigitem(cfg.server, "server");
+  checkconfigitem(cfg.protocolversions, "protocolversions");
+  checkconfigitem(cfg.source, "source");
+  checkconfigitem(cfg.bitsofentropy, "bitsofentropy");
+
+  cfg.minrequestbytes = cfg.minrequestbytes || DEFAULT_MINREQUESTBYTES;
+  cfg.maxrequestbytes = cfg.maxrequestbytes || DEFAULT_MAXREQUESTBYTES;
+  cfg.ratelimits = cfg.ratelimits || [];
+
+  cfg.buffersize = cfg.buffersize || DEFAULT_BUFFERSIZE;
+  cfg.port = cfg.port || process.env.PORT || DEFAULT_PORT;
+
+  return cfg;
+}
+
+function checkconfigitem(item, text) {
+  if (item === null || item === undefined) {                  // if this item doesn't exist then error and exit
+    console.log('Error, "' + text + '": missing in config file.');
+    process.exit(1);
+  }
+}
+
+
+/*** RNG ***/
+
+function initialiseRNG() {
+  ftdi.find(0x0403, 0x6014, function(err, devices) {
+    var device = new ftdi.FtdiDevice(devices[0]);
+
+    device.on('error', function(err) {});                     // set up the error handler - TODO write one!
+
+    device.open({}, function(err) {
+
+      device.on('data', recvdata);                            // set up the new data handler
+      device.write([0xe0, 0x96], function(err) { });          // issue start and stop command
+
+    });
+
+  });
+}
+
+function recvdata(data) {                                     // used to ensure we send data to the correct object
+  circ.push(data);
+}
+
+
+/*** CIRCULAR BUFFER to handle the RNG data ***/
+
+function CircularBuffer(initialsize) {
+  this._data = new Array(initialsize);
+  this._last = initialsize - 1;
+  this._readindex = 0;
+  this._dataindex = 0;
+  // the two index pointers can be equal in one of two states, one where the buffer is full and one where it is empty
+  // when _datafirst == true then either _dataindex > _readindex or _dataindex == _readindex and the buffer is empty
+  // when _datafirst == false then either _dataindex < _readindex or _dataindex == _readindex and the buffer is full
+  this._datafirst = true;                                     // _dataindex is running ahead of the _readindex
+  this._mutex = 0;                                            // used for mutex operations
+}
+
+CircularBuffer.prototype.bytesremaining = function() {         // not mutex protected !!
+  var b;
+
+  if (this._datafirst) {
+    b = this._dataindex - this._readindex;                    // simple case - _dataindex to the end
+  } else {
+    b = (this._last - this._readindex) + 1 + this._dataindex; // add the two bits at the front and back
+  }
+  return b;
+}
+
+CircularBuffer.prototype.push = function(newdata) {
+//  console.log("push called with " + newdata.length.toString() + " bytes");
+  this._usemutex(this._mutexpush, [newdata]);
+//  console.log("finished pushing.  _dataindex: " + this._dataindex.toString());
+  return this;
+}
+
+CircularBuffer.prototype._mutexpush = function(d) {
+  var i, l1, l2;
+
+  if (this._datafirst)  {                                     // fill from _dataindex to end of array
+    l1 = d.length > (this._last - this._dataindex) ? (this._last - this._dataindex) : d.length;  // the maximum amount of data we can receive depends on the remaining space in the buffer
+
+    for (i = 0; i < l1; i++) {
+      this._data[this._dataindex + i] = d[i];                 // read data into the buffer, ensuring we don't write too much
+    }
+
+    this._dataindex += l1;                                    // increment data pointer for new data
+
+    if (l1 < d.length && this._readindex > 0) {               // if there is more data and readindex > 0 then we have can overwrite the used part at the front of the array
+      l2 = (d.length - l1) > this._readindex ? this._readindex : (d.length - l1);   // do we have enough bytes left to fill all of the missing data
+
+      for (i = 0; i < l2; i++) {                              // fill the start of the array
+        this._data[i] = d[l1 + i];
+      }
+
+      this._dataindex = l2;                                   // dataindex now sets back behind the readindex
+      this._datafirst = false;                                // cycle is complete
+    }
+
+  } else {
+
+    l1 = (this._readindex - this._dataindex) > d.length ? d.length : (this._readindex - this._dataindex);  // how much data do we need from the array to fill the buffer
+
+    for (i = 0; i < l1; i++) {
+      this._data[this._dataindex + i] = d[i];
+    }
+
+    this._dataindex += l1;                                    // increment data pointer for new data
+  }
+}
+
+CircularBuffer.prototype.pop = function (count) {
+  var ret = false;
+
+//  console.log("pop called for " + count.toString() + " bytes");
+
+  // TODO it might be better without the following line and just let the more aggressive line after it handle this case
+  // wait until enough data - there is a danger this never completes if request is large and smaller requests keep taking the data as it is added
+  while (count > this.bytesremaining()) { 
+    console.log("Need " + count.toString() + " bytes but only " + this.bytesremaining() + " remaining.");
+  }
+
+  while (ret === false) {
+    ret = this._usemutex(this._mutexpop, [count]);            // keep trying it until it succeeds
+//    console.log("_mutexpop returned " + ret.toString());
+  }
+  return ret;
+}
+
+
+CircularBuffer.prototype._mutexpop = function(count) {
+  var d, i, endbit;
+
+  if (count > this.bytesremaining()) {
+    return false;                                             // not quick enough getting here so fo back and try again
+  } else {
+
+    d = new Array(count);                                     // create array to hold the output to return
+    endbit = this._last - this._readindex;                    // the first part we get
+
+
+    if (this._datafirst || (count <= endbit) ) {              // simple case, just grab what we need from the _readindex onwards
+      for (i = 0; i < count; i++) {
+        d[i] = this._data[this._readindex + i];               // copy the data
+      }
+      this._readindex += count;                               // increment the _readindex
+
+    } else {                                                  // will need to get the data in two bits, first from the end and then from the front
+
+      for (i = 0; i < endbit; i++) {                          // grab it all from _readindex until the end
+        d[i] = this._dataindex[this._readindex + i];
+      }
+
+      for (i = 0; i < (count - endbit); i++) {                // grab the remainder off the front
+        d[i + endbit] = this._data[i];
+      }
+
+      this._readindex = count - endbit;                       // reset the _readindex round to the back
+      this._datafirst = true;                                 // change state
+
+    }
+  }
+  return d;
+}
+
+CircularBuffer.prototype._usemutex = function(func, params) {
+  var succeed = false;
+  var ret;
+
+  while (!succeed) {
+    while (this._mutex > 0) {                                   // wait until the mutex is free
+      console.log("waiting for mutex");
+    } 
+    this._mutex++;                                              // increment the mutex
+    if (this._mutex > 1) {                                      // double lock so back off
+      // TODO may need random sleep here
+      this._mutex--;
+    } else {
+      ret = func.apply(this, params);
+      succeed = true;
+      this._mutex--;
+    }
+  }
+
+  return ret;
+}
